ef0a8fbf751e170a1decf1778eaf9fe5928ace29
jonathansafa <jonathan.f.safa@gmail.com>
2018-05-07 09:13:18 -0700

    added google to header

--- head.php
+++ head.php
@@ -1,5 +1,6 @@
 <head>
     <meta charset="utf-8">
+    <meta name="google-site-verification" content="JLrrNXFFGWTnLPLvlEbUEX9EDBhb7I7z6MJikuKZHsM" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Closed Captions, Encoding, Delivery to Network - $99 ONLY">
     <meta name="author" content="www.zipcaptions.com">
