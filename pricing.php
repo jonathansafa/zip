c0ea338bc2632a8fb57783477a09cef384c48e22
DHSprout <hpsprout15@gmail.com>
2018-06-20 22:01:57 +0530

    pricing changes

--- pricing.php
+++ pricing.php
@@ -17,7 +17,7 @@
                         <div class="col-xs-12">
                             <h1 class="Pricing-Header-title zip-pricing-PageTitle">
                                 Simple, wallet-friendly pricing.
-                                <span class="Pricing-Header-subTitle zip-pricing-PageSubtitle">Easy. Hidden pricing.</span>
+                                <span class="Pricing-Header-subTitle zip-pricing-PageSubtitle">Easy. Affordable. Fast.</span>
                             </h1>
                         </div>
                     </div>
@@ -45,12 +45,8 @@
                                         Include SSC, MCC, or a number of other formats.
                                     </li>
                                     <li class="price-listItem">
-                                        <img src="<?= SITE_URL ?>/img/plan-receive.svg" width="26" height="26" alt="pay icon">
-                                        Pay only for what you use
-                                    </li>
-                                    <li class="price-listItem">
-                                        <img src="<?= SITE_URL ?>/img/plan-measure.svg" width="26" height="26" alt="reporting icon">
-                                        Real-time fee reporting
+                                        <img src="<?= SITE_URL ?>/img/fcc.png" width="26" height="26" alt="pay icon">
+                                        All our captions are fully FCC Compliant. 
                                     </li>
                                 </ul>
                                 <a href="javascript:void(0)"  class="price-button zip-pricing-Uppercase zip-pricing-Link-arrow open-nav">
@@ -83,7 +79,7 @@
                                         </tbody>
                                     </table>
                                 </div>
-                                <a href="<?= SITE_URL ?>/contact" class="price-button price-Button-enterprise zip-pricing-Uppercase zip-pricing-Link-arrow">Contact Sales</a>
+                                <a href="javascript:void(0)" onclick="openChat();" class="price-button price-Button-enterprise zip-pricing-Uppercase zip-pricing-Link-arrow">Contact Sales</a>
                             </div>
                         </div>
                     </div>
@@ -93,13 +89,13 @@
                 <div class="container">
                     <div class="row center-xs">
                         <div class="col-xs col-sm-12 col-md-9">
-                            <img src="<?= SITE_URL ?>/img/transfers.svg" width="48" height="53" alt="transfers logo">
+                            <img src="<?= SITE_URL ?>/img/stopwatch.png" width="48" height="53" alt="stop watch">
                             <p class="zip-pricing-BodyText TransfersAside-body">
                                 <strong class="TransfersAside-title-head">ALL FILES: 72 hours or less</strong>
                             </p>
                             <p class="zip-pricing-BodyText TransfersAside-body">
-                                <strong class="TransfersAside-title">Fast, predictable transfers.</strong>
-                                Once you’re set up, transfers arrive in your bank account on a 2-day rolling basis. Or you can opt to receive transfers weekly or monthly.
+                                <strong class="TransfersAside-title">Fast transfers.</strong>
+                                We guarantee our captions arrive accurately and on time, every time.
                             </p>
                         </div>
                     </div>
@@ -108,26 +104,16 @@
             <footer class="price-footer withCards">
                 <section class="price-footer-sections">
                     <div class="container-xl">
-                        <a class="zip-pricing-Link price-footer-section card-customers" href="/us/customers" data-analytics-action="customers" data-analytics-source="card_link">
-                            <svg viewBox="0 0 130 130">
-                            <path class="hover-fill-blak-white" fill="#87BBFD" d="M63.97 115.5H16.92a2.5 2.5 0 0 1-2.5-2.5V69.52c0-14.37 11.65-26.02 26.02-26.02h.006c14.37 0 26.02 11.65 26.02 26.02V113a2.5 2.5 0 0 1-2.5 2.5zm-23.47-58a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19z"></path>
-                            <path class="hover-fill-color" fill="#6772E5" d="M109.5 115.5H91V103c0-.83-.67-1.5-1.5-1.5h-7c-.83 0-1.5.67-1.5 1.5v12.5H62.5A2.5 2.5 0 0 1 60 113V29.5a5 5 0 0 1 5-5h6.3c1.39-6.846 7.443-12 14.7-12s13.31 5.154 14.7 12h6.3a5 5 0 0 1 5 5V113a2.5 2.5 0 0 1-2.5 2.5z"></path>
-                            <path class="hover-fill-blak-white" fill="#87BBFD" d="M104.5 70.5h-7A1.5 1.5 0 0 1 96 69V58c0-.828.67-1.5 1.5-1.5h7c.83 0 1.5.672 1.5 1.5v11c0 .83-.67 1.5-1.5 1.5zm-15 22h-7A1.5 1.5 0 0 1 81 91V80c0-.83.67-1.5 1.5-1.5h7c.83 0 1.5.67 1.5 1.5v11c0 .83-.67 1.5-1.5 1.5zm0-44h-7A1.5 1.5 0 0 1 81 47V36c0-.828.67-1.5 1.5-1.5h7c.83 0 1.5.672 1.5 1.5v11a1.5 1.5 0 0 1-1.5 1.5zm-15 44h-7A1.5 1.5 0 0 1 66 91V80c0-.83.67-1.5 1.5-1.5h7c.83 0 1.5.67 1.5 1.5v11c0 .83-.67 1.5-1.5 1.5zm0-44h-7A1.5 1.5 0 0 1 66 47V36c0-.828.67-1.5 1.5-1.5h7c.83 0 1.5.672 1.5 1.5v11a1.5 1.5 0 0 1-1.5 1.5z"></path>
-                            <path fill="#FFF" d="M104.5 92.5h-7A1.5 1.5 0 0 1 96 91V80c0-.83.67-1.5 1.5-1.5h7c.83 0 1.5.67 1.5 1.5v11c0 .83-.67 1.5-1.5 1.5zm-30-22h-7A1.5 1.5 0 0 1 66 69V58c0-.828.67-1.5 1.5-1.5h7c.83 0 1.5.672 1.5 1.5v11c0 .83-.67 1.5-1.5 1.5z"></path>
-                            </svg>
+                        <a class="zip-pricing-Link price-footer-section card-customers">
+                            <img src="<?= SITE_URL ?>/img/business.png"/>
                             <h2 class="zip-pricing-Uppercase zip-pricing-Link-arrow">See our customers</h2>
-                            <p class="zip-pricing-BodyText">From startups to Fortune 500s, explore how 100,000+ businesses use Stripe to start and scale their companies.</p>
+                            <p class="zip-pricing-BodyText">From affiliate stations to the 3rd largest broadcast group in U.S. See some of the video producers that use zipcations.com to deliver their content.</p>
                         </a>
 
-                        <a class="zip-pricing-Link price-footer-section card-documentation" href="/docs" data-analytics-action="documentation" data-analytics-source="card_link">
-                            <svg viewBox="0 0 130 130">
-                            <path class="hover-fill-color" fill="#9251AC" d="M113 112H61.36c-2.224 0-4.466-1.574-5.27-3.277C54.005 104.313 46.457 102 37 102H5a5 5 0 0 1-5-5V25a5 5 0 0 1 5-5h33c11.045 0 20 4.477 20 10v37h60v40a5 5 0 0 1-5 5z"></path>
-                            <path class="hover-fill-blak-white" fill="#F6A4EB" d="M121.993 93c0 6-12.363 10.997-27.384 10.997h-3.464v.01h-23.61c-1.823 0-5.27.637-6.82 1.627-.59.377-1.285.544-1.893.195a1.632 1.632 0 0 1-.817-1.416l.003-74.91c0-4.14 4.815-7.5 10.755-7.5h10.51c.075-.002.148-.008.224-.008h13.17c8.922 0 20.682-2.238 25.62-5.573 1.587-1.072 3.702-.09 3.702 1.82 0 0 .007 67.756 0 74.756z"></path>
-                            <path stroke="#FFF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" d="M80.5 86.5h26m-26-10h26m-26-17h26m-26-10h26m-26-10h26"></path>
-                            <path stroke="#FFF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" fill="none" d="M68.992 78.007l3.516 3.494-3.516 3.496m0-43.99l3.516 3.495-3.516 3.496"></path>
-                            </svg>
-                            <h2 class="zip-pricing-Uppercase zip-pricing-Link-arrow">Explore the docs</h2>
-                            <p class="zip-pricing-BodyText">Start building your integration and accept your first payment in minutes. Stripe libraries are available in every language from Ruby to Go.</p>
+                        <a class="zip-pricing-Link price-footer-section card-documentation">
+                            <img src="<?= SITE_URL ?>/img/icon-documents.png"/>
+                            <h2 class="zip-pricing-Uppercase zip-pricing-Link-arrow">Download our required specs</h2>
+                            <p class="zip-pricing-BodyText">Start prepping your video and begin captioning your first video in minutes.</p>
                         </a>
 
 
@@ -140,19 +126,18 @@
                         <div class="content">
                             <h1 class="title">
                                 <span class="subtitle">Ready to get started?</span>
-                                Get in touch, or create an account.
+                                Get in touch, we'd love to speak.
                             </h1>
                         </div>
 
                         <div class="zip-pricing-btn-grp buttons">
 
-                            <a class="zip-pricing-Button zip-pricing-Button-default" href="<?= SITE_URL ?>/contact" data-analytics-action="sign_up" data-analytics-source="footer_cta">
-                                Create Stripe account
+                            <a class="zip-pricing-Button zip-pricing-Button-default" href="<?= SITE_URL ?>/contact">
+                                SEND AN EMAIL
                             </a>
 
-                            <a href="<?= SITE_URL ?>/contact" class="zip-pricing-Button" data-analytics-action="contact_sales" data-analytics-source="footer_cta">
-                                Contact Sales
-
+                            <a href="javascript:void(0)" class="zip-pricing-Button" onclick="openChat();">
+                                CHAT WITH SALES
                             </a>
                         </div>
 
